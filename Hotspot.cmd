f00c1165207f47b0dce0c46cf57aa0aeee99ac76
JamesCullum <nicolas.giese@web.de>
2016-02-26 17:15:10 +0100

    Added autostart and instructions
    Differentiates now between setup and activation, can create an autostart
    entry for the hotspot, added instructions on how to configure the
    hotspot on first run

--- Hotspot.cmd
+++ Hotspot.cmd
@@ -3,26 +3,50 @@
 :start
 cls
 echo Choose an action below by entering its number.
-echo [1] Activate Hotspot
-echo [2] Deactivate Hotspot
-echo [3] List connected devices
+echo [1] Setup or Modify
+echo [2] Activate Hotspot
+echo [3] Deactivate Hotspot
+echo [4] List connected devices
 set /p choice=
-if %choice%==1 goto activate
-if %choice%==2 goto deactivate
-if %choice%==3 goto manage
+if %choice%==1 goto setup
+if %choice%==2 goto activate
+if %choice%==3 goto deactivate
+if %choice%==4 goto manage
 echo Unknown action, please just enter the number.
 pause
 goto start
 
-:activate
+:setup
 netsh wlan stop hostednetwork >NUL
 echo Enter the SSID of the hotspot
 set /p set_ssid=
 echo Enter the password for accessing the hotspot (8 characters minimum)
 set /p set_pass=
+echo Create a [t]emporary or [p]ermanent hotspot?
+echo Temporary hotspots vanish after a restart
+echo Enter the marked letter of your prefered option
+set /p set_permanent=
 netsh wlan set hostednetwork mode=allow ssid="%set_ssid%" key="%set_pass%"
 netsh wlan start hostednetwork
 netsh wlan show hostednetwork setting=security
+if "%set_permanent%"=="p" (
+	echo netsh wlan start hostednetwork > "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\permanent_hotspot.cmd"
+	echo Hotspot set to permanent mode
+)
+if "%set_permanent%"=="t" (
+	del /f /q "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\permanent_hotspot.cmd" >NUL
+	echo Hotspot set to temporary mode
+)
+echo ------------------
+echo If this is your first setup you need to open up the adapter settings in the Network and Sharing Center
+echo Select your working internet connection, right click on it, go to Properties
+echo Switch to the tab called Sharing, tick to share the connection and select your created hotspot
+echo ------------------
+pause
+goto start
+
+:activate
+netsh wlan start hostednetwork
 pause
 goto start
 
